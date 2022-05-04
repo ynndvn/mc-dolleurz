eef64a12d842c46e6b0deb3f2b569b0d62d9155c
Yann Davin <yanndvn@gmail.com>
2022-05-04 19:35:47 +0200

    fix ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,12 @@ module.exports = {
   apps: [
     {
       name: "MCBitcoin",
-      script: "/app/mc-dollarz/dist/app.js",
+      script: "/app/mc-dolleurz/dist/app.js",
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: "1G",
-      cwd: "/app/mc-dollarz",
+      cwd: "/app/mc-dolleurz",
       exec_mode: "fork"
     },
   ],
